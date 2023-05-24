b266a08013ed22e98746e32ae02a1339a76bd3ab
EdanPrince <112059492+EdanPrince@users.noreply.github.com>
2023-05-24 08:58:58 +0700

    Add files via upload

---
+++ Test2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tạo một đối tượng Dog
+            Chicken d = new Chicken();
+
+            // Tạo một đối tượng Cat
+            Pig c = new Pig();
+
+            // Gọi phương thức Speak của d và c
+            d.Speak();
+            c.Speak();
+        }
+    }
+}
